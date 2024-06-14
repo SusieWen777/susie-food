7abe92d8b2b0e7dad7183c74775bc099fa55542a
SusieWen777 <wen_bufan11@163.com>
2024-06-14 15:27:27 +1000

    Complete the header and explore-menu part of the homepage

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Welcome to Susie Food, your go-to destination for delicious and
+          convenient food delivery. Whether you&apos;re craving a quick snack or
+          a gourmet meal, we&apos;ve got you covered. Browse through our
+          extensive menu, place your order, and enjoy fresh, hot meals delivered
+          right to your doorstep. Your favorite dishes are just a click away!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import Header from "../../components/Header/Header";
 import "./Home.scss";
 
 function Home() {
-  return <div>Home</div>;
+  const [category, setCategory] = useState("All");
+
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  );
 }
 
 export default Home;
