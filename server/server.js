38555415e0dda71fecc02062a3b955d85f7f8e78
SusieWen777 <wen_bufan11@163.com>
2024-06-20 15:51:55 +1000

    Create backend for placing order

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/useRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 dotenv.config();
@@ -22,6 +23,8 @@ connectDB();
 app.use("/api/food", foodRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
 // app.use("/images", express.static("uploads"));
 
 app.get("/", (req, res) => {
